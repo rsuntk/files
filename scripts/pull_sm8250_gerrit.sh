5df5ac5d6d109bb84b3ef256cf738f7c732082bc
rsuntk <rissu.ntk@gmail.com>
2026-03-04 13:12:07 +0700

    [scripts]: fixup! local only can be used in a function
    Signed-off-by: rsuntk <rissu.ntk@gmail.com>

--- scripts/pull_sm8250_gerrit.sh
+++ scripts/pull_sm8250_gerrit.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/env bash
 
 REFS="$1"
-local start_time=$(date +"%s")
+start_time=$(date +"%s")
 
 if [ -z "$REFS" ]; then
 	echo "Specify the refs!"
@@ -21,8 +21,8 @@ echo "To be pulled: $REFS"
 [ ! -d $(pwd)/sm8250 ] && git clone https://github.com/LineageOS/android_kernel_qcom_sm8250.git sm8250 || echo "No need to reclone."
 
 pushd $(pwd)/sm8250; git fetch https://github.com/LineageOS/android_kernel_qcom_sm8250 "$REFS" && git reset --hard FETCH_HEAD; popd
-local end_time=$(date +"%s")
-local tdiff=$(($end_time-$start_time))
-local mins=$((($tdiff % 3600) / 60))
-local secs=$(($tdiff % 60))
+end_time=$(date +"%s")
+tdiff=$(($end_time-$start_time))
+mins=$((($tdiff % 3600) / 60))
+secs=$(($tdiff % 60))
 printf "Done, pulled $REFS (%02g:%02g (mm:ss))\n" $mins $secs
