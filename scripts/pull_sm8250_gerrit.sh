5478c38d927eadf4c5badbd429f6272c2144132e
rsuntk <rissu.ntk@gmail.com>
2026-03-04 13:09:45 +0700

    [scripts]: fixup! no need to reclone repository if its already exist
    Signed-off-by: rsuntk <rissu.ntk@gmail.com>

--- scripts/pull_sm8250_gerrit.sh
+++ scripts/pull_sm8250_gerrit.sh
@@ -18,12 +18,11 @@ fi
 
 echo "To be pulled: $REFS"
 
-git clone https://github.com/LineageOS/android_kernel_qcom_sm8250.git sm8250
-pushd $(pwd)/sm8250; git fetch https://github.com/LineageOS/android_kernel_qcom_sm8250 "$REFS" && git reset --hard FETCH_HEAD; popd
+[ ! -d $(pwd)/sm8250 ] && git clone https://github.com/LineageOS/android_kernel_qcom_sm8250.git sm8250 || echo "No need to reclone."
 
+pushd $(pwd)/sm8250; git fetch https://github.com/LineageOS/android_kernel_qcom_sm8250 "$REFS" && git reset --hard FETCH_HEAD; popd
 local end_time=$(date +"%s")
 local tdiff=$(($end_time-$start_time))
 local mins=$((($tdiff % 3600) / 60))
 local secs=$(($tdiff % 60))
-
 printf "Done, pulled $REFS (%02g:%02g (mm:ss))\n" $mins $secs
