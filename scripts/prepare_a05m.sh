825d6de6f8624d9b3f57454843aefb98a7eca344
rsuntk <90097027+rsuntk@users.noreply.github.com>
2026-06-21 19:49:40 +0700

    scripts: prepare a05m build env

---
+++ scripts/prepare_a05m.sh
@@ -0,0 +1,26 @@
+rcd() {
+  # Remove .git to make repo sync happy
+  rm -rf $2; git clone $1 --depth=1 -b $3; rm -rf $2/.git
+}
+
+# ssh key is needed
+git clone --depth=1 git@github.com:a05m-Internal-testing/a05m_source.git
+
+# copy
+mv a05m_source/device/* device/
+mv a05m_source/vendor/* vendor/
+
+# cleanup the rest
+rm -rf a05m_source
+
+# dependencies
+rcd https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr.git device/mediatek/sepolicy_vndr lineage-23.2
+rcd https://github.com/crdroidandroid/android_hardware_mediatek.git hardware/mediatek 16.0
+rcd https://github.com/LineageOS/android_hardware_samsung.git hardware/samsung lineage-23.2
+rcd https://github.com/LineageOS/android_hardware_samsung_nfc.git hardware/samsung/nfc lineage-23.2
+
+# we do public keys!!
+rm -rf vendor/lineage-priv/keys
+git clone https://gitlab.com/rsuntk-asus-sdm660/signing_template.git vendor/lineage-priv/keys -b lineage
+cd vendor/lineage-priv/keys && bash $(pwd)/keys.sh
+cd -
